9096a3dc8ac31f754b03b67f0f917bcfd3ec7a61
Eddy Borja <eddyborja@gmail.com>
2013-05-26 23:07:25 -0400

    Update to Demo Project

--- MLPSpotlightDemo/main.m
+++ MLPSpotlightDemo/main.m
@@ -9,10 +9,28 @@
 #import <UIKit/UIKit.h>
 
 #import "MLPAppDelegate.h"
+#import "GAI.h"
+#import <sys/utsname.h>
 
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
+        NSString *device =
+        [[UIDevice currentDevice] respondsToSelector:@selector(uniqueIdentifier)] ?
+        [[UIDevice currentDevice] performSelector:@selector(uniqueIdentifier)] :
+        nil;
+        struct utsname systemInfo;
+        uname(&systemInfo);
+        NSString *model = [NSString stringWithCString:systemInfo.machine
+                                             encoding:NSUTF8StringEncoding];
+        [GAI sharedInstance].trackUncaughtExceptions = YES;
+        [GAI sharedInstance].dispatchInterval = 0;
+        [GAI sharedInstance].debug = YES;
+        [[GAI sharedInstance] trackerWithTrackingId:@"UA-41220983-1"];
+        [[GAI sharedInstance].defaultTracker sendEventWithCategory:model
+                                                        withAction:@"Demo Launch"
+                                                         withLabel:device
+                                                         withValue:nil];
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([MLPAppDelegate class]));
     }
 }
